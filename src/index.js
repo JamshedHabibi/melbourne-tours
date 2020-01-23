90111f3cdf1e4a56fc87dcec46a287cff53024f0
Jamshed Habibi <jamshed.habibi100@gmail.com>
2020-01-23 19:10:42 +0000

    fix for page break on refresh

--- src/index.js
+++ src/index.js
@@ -3,16 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {Provider} from './Context'
-import {BrowserRouter as Router} from 'react-router-dom'
+import {Provider} from './Context';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 ReactDOM.render(
-<Provider>
-    <Router>
-        <App /> 
-    </Router>
-</Provider>,
-document.getElementById('root'));
+	<Provider>
+		<Router>
+			<App />
+		</Router>
+	</Provider>,
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
