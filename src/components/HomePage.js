616341ebdc139d3e48155b123d8e912e53521400
Jamshed Habibi <jamshed.habibi@davidlloyd.co.uk>
2021-10-28 12:10:49 +0100

    Updated css to correctly fit large screens

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,40 +1,36 @@
-import React, {Component} from 'react';
-import {Grid} from 'semantic-ui-react';
+import React, { Component } from 'react';
+import { Grid } from 'semantic-ui-react';
 import './HomePage.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import NavbarHomePage from './NavbarHomePage';
 
 export default class HomePage extends Component {
-	render() {
-		return (
-			<div className="homepage">
-				<div>
-					<img className="background-img" width="1800" src="background.jpg" alt="" />
-				</div>
+  render() {
+    return (
+      <div className="homepage">
+        <img className="background-img" src="background.jpg" alt="" />
 
-				<Grid className="homepage-content">
-					<Grid.Column width={16}>
-						<Grid.Row className="homepage-main-title">
-							<p className="homepage-main-title">Turn a trip into an adventure</p>
-						</Grid.Row>
+        <Grid className="homepage-content">
+          <Grid.Column width={16}>
+            <Grid.Row className="homepage-main-title">
+              <p className="homepage-main-title">Turn a trip into an adventure</p>
+            </Grid.Row>
 
-						<Grid.Row className="homepage-subtitle">
-							<p className="homepage-subtitle">
-								Unique experiences you'll want to tell the world about
-							</p>
-						</Grid.Row>
+            <Grid.Row className="homepage-subtitle">
+              <p className="homepage-subtitle">Unique experiences you'll want to tell the world about</p>
+            </Grid.Row>
 
-						<Grid.Row>
-							<Link as="div" to="/tours">
-								<div className="homepage-find-tours-button">
-									<h5>FIND OUT MORE</h5>
-								</div>
-							</Link>
-						</Grid.Row>
-					</Grid.Column>
-				</Grid>
-				<NavbarHomePage />
-			</div>
-		);
-	}
+            <Grid.Row>
+              <Link as="div" to="/tours">
+                <div className="homepage-find-tours-button">
+                  <h5>FIND OUT MORE</h5>
+                </div>
+              </Link>
+            </Grid.Row>
+          </Grid.Column>
+        </Grid>
+        <NavbarHomePage />
+      </div>
+    );
+  }
 }
